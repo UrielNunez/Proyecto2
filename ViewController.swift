0fd36aa23e83f67e9cfb31c0acf5240ca4b7023f
Uriel Nuñez <Devuriel1@gmail.com>
2022-05-06 17:07:15 -0700

    primer commit

---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Proyecto2
+//
+//  Created by Omar Uriel Flores Nuñez on 06/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
