19ffd7abb9123b1053320bb777e54257374fd149
Uriel Nuñez <Devuriel1@gmail.com>
2022-05-19 16:41:25 -0700

    Update: Ahorcado

---
+++ Proyecto2/ViewControllerDificultPreguntados.swift
@@ -0,0 +1,27 @@
+//
+//  ViewControllerDificultPreguntados.swift
+//  Proyecto2
+//
+//  Created by Omar Uriel Flores Nuñez on 18/05/22.
+//
+
+import UIKit
+
+class ViewControllerDificultPreguntados: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Proyecto2/ViewControllerJuegos.swift
@@ -0,0 +1,31 @@
+//
+//  ViewControllerJuegos.swift
+//  Proyecto2
+//
+//  Created by Omar Uriel Flores Nuñez on 18/05/22.
+//
+
+import UIKit
+
+class ViewControllerJuegos: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //ESTO ES PARA QUITAR EL BOTON DE BACK QUE SE TIENE EN UNA VIEW CONTROLLER
+        //UTILIZAR DESPUES
+        navigationItem.hidesBackButton = true
+
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
